af749692099bcaea4996cd8aae1ad57185300699
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-12-16 17:15:07 +0530

    -y

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,70 @@
-struct User{
-    active:bool,
-    username:String,
-    email:String,
-    sign_in_count:u64,
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
 #[derive(Debug)]
-struct Rectangle{
-    width:u32,
-    height:u32,
-
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn main(){
+fn main() {
     // Creating Instance of Struct
-    let mut user1 = User{
-        active:true,
-        username:String::from("Saksham"),
-        email:String::from("sakshamg45@gmail.com"),
-        sign_in_count:1
+    let mut user1 = User {
+        active: true,
+        username: String::from("Saksham"),
+        email: String::from("sakshamg45@gmail.com"),
+        sign_in_count: 1,
     };
 
     // To update the value using (.) dot operator
     // user1.email = String::from("sakshamgupta.ai@gmail.com");
 
-    build_user("Krishna".to_string(),"krishnathegod@loveall.com".to_string());
+    build_user(
+        "Krishna".to_string(),
+        "krishnathegod@loveall.com".to_string(),
+    );
 
     // Using Struct Update Syntax
-    let user2 = User{
+    let user2 = User {
         email: String::from("sakshamgupta.ai@gmail.com"),
         username: String::from("Yogi Saksham"),
         ..user1
     };
 
     // Structs usage for area calculation
-    let rect1 = Rectangle{
-        width:30,
-        height:50,
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
 
-    println!("The Area of Rectangle is {} sq. pixels",area(&rect1));
-    println!("The Rect1 is {:#?}",rect1);
+    println!("The Area of Rectangle is {} sq. pixels", area(&rect1));
+    println!("The Rect1 is {:#?}", rect1);
+
+    // using dgb! macro
+    println!("\n");
+
+    let scale = 2;
+    let rect2 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect2);
 }
 
-
 // Field Init Shorthand used here
-fn build_user(username:String,email:String)-> User{
-    User{
-        active:true,
+fn build_user(username: String, email: String) -> User {
+    User {
+        active: true,
         username,
         email,
-        sign_in_count:1
+        sign_in_count: 1,
     }
 }
 
-fn area(rectangle: &Rectangle)->u32{
+fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
